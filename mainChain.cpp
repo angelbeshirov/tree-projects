cdfd085b2204af7718a31d951d14920d11d96493
angelbeshirov <32986379+angelbeshirov@users.noreply.github.com>
2018-01-30 01:52:33 +0200

    tree projects

---
+++ mainChain.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<fstream>
+#include"ChainTree.h"
+using namespace std;
+/*
+vector<Player> readFromFile(){
+    srand(time(0));
+    vector<Player> testVec;
+    vector<Player> realVec;
+    Player player;
+    ifstream object;
+    object.open("participants.txt",ios::in);
+
+    while(object>>player.name>>player.coef){
+        testVec.push_back(player);
+    }
+
+    int rndNumber;
+    int howManyPlayersToReallyInclude = rand() % testVec.size();
+
+    while(howManyPlayersToReallyInclude-- >= 0){
+        if(testVec.size() == 1){
+            rndNumber = 0;
+        } else {
+            rndNumber = rand() % (testVec.size());
+        }
+        realVec.push_back(testVec[rndNumber]);
+        testVec.erase(testVec.begin() + rndNumber);
+    }
+    object.close();
+    return realVec;
+};
+
+void allFunctionTogether(){
+    ChainTree<Player> k;
+    vector<Player> players = readFromFile();
+    k.create(players);
+    k.tournament();
+    cout<<endl;
+    string name;
+    cout<<endl;
+    cout<<"Enter name to search for:";cin>>name;
+    k.findpath(name);
+};
+
+int main(){
+    allFunctionTogether();
+
+};*/
